a8313369e39db40e557d2438cb20be1812c47608
sadanand <saddanand@gmail.com>
2017-12-25 19:24:01 +0530

    implemented: xjc, JAXB, MT-dataRetrievalSerivce

--- BalanceComparison/src/main/java/com/balance/comparison/model/BalanceComparisonRequest.java
+++ BalanceComparison/src/main/java/com/balance/comparison/model/BalanceComparisonRequest.java
@@ -1,14 +1,24 @@
 package com.balance.comparison.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * POJO to mapping client JSON request into java object
  * @author Tanu
  *
  */
 public class BalanceComparisonRequest {
-	
+
 	private String name;
 
+	private int dateFrom;
+	private int dateTo;
+	private String measure;
+	private List<String> sources = Arrays.asList("icici","sbi");
+
+	
+	
 	public String getName() {
 		return name;
 	}
@@ -17,6 +27,35 @@ public class BalanceComparisonRequest {
 		this.name = name;
 	}
 	
-	
+	public int getDateFrom() {
+		return dateFrom;
+	}
 
+	public void setDateFrom(int dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public int getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(int dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	public String getMeasure() {
+		return measure;
+	}
+
+	public void setMeasure(String measure) {
+		this.measure = measure;
+	}
+
+	public List<String> getSources() {
+		return sources;
+	}
+
+	public void setSources(List<String> sources) {
+		this.sources = Arrays.asList("icici","sbi");
+	}
 }
--- BalanceComparison/src/main/java/com/balance/comparison/rest/BalanceComparisonRestService.java
+++ BalanceComparison/src/main/java/com/balance/comparison/rest/BalanceComparisonRestService.java
@@ -1,5 +1,7 @@
 package com.balance.comparison.rest;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -10,9 +12,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.balance.comparison.executor.DataPartitionerExecutorService;
 import com.balance.comparison.model.BalanceComparisonRequest;
 
 /**
@@ -23,6 +27,13 @@ import com.balance.comparison.model.BalanceComparisonRequest;
    http://localhost:8080/BalanceComparison/compareBalance
  *
  * {"name" : "abc"}
+ 
+ {
+ "dateFrom":201712,
+ "dateFrom":201712,
+ "measure": "all"
+ }
+ *
  */
 
 @Component
@@ -31,6 +42,11 @@ public class BalanceComparisonRestService {
 
 	private static Logger LOG =  Logger.getLogger(BalanceComparisonRestService.class);
 
+	// inject request validator service
+	
+	@Autowired
+	private DataPartitionerExecutorService dataPartitionerExecutorService; 
+	
 	@POST
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
@@ -39,7 +55,26 @@ public class BalanceComparisonRestService {
 
 		LOG.info("test logging");
 
+	// validate input request
+		
+		// call compare all partioon on datapartition service
+		// this will return a final aggregated map 
+		
+		List<String> comparedData = dataPartitionerExecutorService.compareDataForAllPartitons(clientRequest);
+		LOG.info("final compared Data set:  "+comparedData);
 		return Response.status(200).entity("hello world: "+clientRequest.getName()).build();
+		
 	}
 
+	public void setDataPartitionerExecutorService(
+			DataPartitionerExecutorService dataPartitionerExecutorService) {
+		this.dataPartitionerExecutorService = dataPartitionerExecutorService;
+	}
+
+	public DataPartitionerExecutorService getDataPartitionerExecutorService() {
+		return dataPartitionerExecutorService;
+	}
+	
+	
+	
 }
